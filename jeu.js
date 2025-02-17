4dcb0d5985947570973771fd864ee0bb52c186f2
Aymen Abid <aymen.abid20233@gmail.com>
2025-02-17 23:11:07 +0100

    fixing changes

--- jeu.js
+++ jeu.js
@@ -18,7 +18,7 @@ function start(){
 function difficulty(){
     s = start();
     ind = s[0]
-    x = Math.floor(Math.random() * 11);
+    x = Math.floor(Math.random() * Game[ind].end);
     essai = document.getElementById("essai").value;
     if(isNaN(essai) || essai < 0 || essai > Game[ind].end || essai == ""){
         alert("le nombre est n'est pas valide");
@@ -31,6 +31,8 @@ function difficulty(){
         Game[ind].tent-- ;
         alert(`vous avez perdu ! \nIl vous reste ${Game[ind].tent} Tentatives.`);
         if(Game[ind].tent == 0){
+            alert("Le jeu est terminé, essayer de nouveau !");
+            document.getElementById("interval").innerHTML = `${Game[ind].begin} et ${Game[ind].end}, vous avez en total : ${Game[ind].tent}`;
             Game[ind].tent = s[2] - ind + 1;
         }
     }
